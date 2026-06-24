6ed55051005d2ce5a763045600e4976a9ada99b5
Sagararora90 <arorasagar540@gmail.com>
2026-06-25 03:58:46 +0530

    Patch Cloudflare speed test to prevent false no-internet error

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -9,4 +9,11 @@ self.__uv$config = {
     bundle: "/uv/uv.bundle.js",
     config: "/uv/uv.config.js",
     sw: "/uv/uv.sw.js",
+    inject: [
+        {
+            host: ".*",
+            html: '<script>(function(){try{var o=performance.getEntriesByName;performance.getEntriesByName=function(n,t){var r=o.call(performance,n,t);if(r&&r.length)return r;return[{transferSize:1000,encodedBodySize:1000,decodedBodySize:1000,duration:50,startTime:performance.now()-50,responseEnd:performance.now(),name:n,entryType:t||"resource",initiatorType:"fetch"}]}}catch(e){}})()</script>',
+            injectTo: "head"
+        }
+    ]
 };
