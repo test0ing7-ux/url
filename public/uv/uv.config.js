35c57852417ba7a2b3ec230c6f5ccc0d1d1dd0ee
Sagararora90 <arorasagar540@gmail.com>
2026-06-25 04:04:57 +0530

    Inject on-screen error alerts

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -12,7 +12,7 @@ self.__uv$config = {
     inject: [
         {
             host: ".*",
-            html: '<script>(function(){try{var fake=[{transferSize:1000,encodedBodySize:1000,decodedBodySize:1000,duration:50,startTime:0,responseEnd:50,name:"https://speed.cloudflare.com/__down?bytes=0",entryType:"resource",initiatorType:"fetch"}];var o=performance.getEntriesByName;performance.getEntriesByName=function(n,t){var r=o.call(performance,n,t);if(r&&r.length)return r;return fake};var p=performance.getEntries;performance.getEntries=function(){var r=p.call(performance);return r.concat(fake)};}catch(e){}})()</script>',
+            html: '<script>(function(){try{var fake=[{transferSize:1000,encodedBodySize:1000,decodedBodySize:1000,duration:50,startTime:0,responseEnd:50,name:"https://speed.cloudflare.com/__down?bytes=0",entryType:"resource",initiatorType:"fetch"}];var o=performance.getEntriesByName;performance.getEntriesByName=function(n,t){var r=o.call(performance,n,t);if(r&&r.length)return r;return fake};var p=performance.getEntries;performance.getEntries=function(){var r=p.call(performance);return r.concat(fake)};}catch(e){}})()</script><script>window.addEventListener("error", function(e) { alert("APP ERROR: " + e.message + " in " + e.filename); }); window.addEventListener("unhandledrejection", function(e) { alert("APP PROMISE ERROR: " + (e.reason && e.reason.message ? e.reason.message : e.reason)); });</script>',
             injectTo: "head"
         }
     ]
